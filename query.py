d3a05aa1fa735e4c1c85068899494266f93cc864
Jerry Wang <50364307+jerrymark611@users.noreply.github.com>
2021-08-30 23:46:22 +0800

    Add files via upload

--- query.py
+++ query.py
@@ -1,101 +1,85 @@
 # %%
 import mercantile, requests
 from vt2geojson.tools import vt_bytes_to_geojson
-from math import sin, cos, asin, sqrt, radians
-
+from math import sin, cos, asin, sqrt, radians, degrees, atan2
+import json
 # %%
 # get metadata inside an area
-# def query_area(west: float, south: float, east: float, north: float, access_token: str):
-#     # define an empty geojson as output
-#     output = {"type": "FeatureCollection", "features": []}
+def query_area(access_token: str, west: float, south: float, east: float, north: float):
+    tile_coverage = 'mly1_public'
+    tile_layer = "image"
+    output= { "type": "FeatureCollection", "features": [] } 
 
-#     # vector tile endpoints -- change this in the API request to reference the correct endpoint
-#     tile_coverage = 'mly1_public'
 
-#     # tile layer depends which vector tile endpoints:
-#     # 1. if map features or traffic signs, it will be "point" always
-#     # 2. if looking for coverage, it will be "image" for points, "sequence" for lines, or "overview" for far zoom
-#     tile_layer = "image"
+    # west, south, east, north = 120.43976795391002, 23.525173201509304, 120.4875756473915, 23.5965857805608
+    tiles = list(mercantile.tiles(west, south, east, north, 14))
+    
 
-#     # Mapillary access token -- user should provide their own
+    # loop through list of tiles to get tile z/x/y to plug in to Mapillary endpoints and make request
+    for i, tile in enumerate(tiles):
+        print(f"{i}/{len(tiles)}")
+        tile_url = 'https://tiles.mapillary.com/maps/vtp/{}/2/{}/{}/{}?access_token={}'.format(tile_coverage,tile.z,tile.x,tile.y,access_token)
+        response = requests.get(tile_url)
+        data = vt_bytes_to_geojson(response.content, tile.x, tile.y, tile.z,layer=tile_layer)
+        
 
-#     # west, south, east, north = [-80.12823442840576, 25.77376933762778, -80.1264238357544, 25.788608487732198]
+        for feature in data['features']:
+            if feature['properties']['is_pano']:
+                output['features'].append(feature)
+    with open('images.geojson', 'w') as f:
+        json.dump(output, f)
+    return output
 
-#     # get the list of tiles with x and y coordinates which intersect our bounding box
-#     # MUST be at zoom level 14 where the data is available, other zooms currently not supported
-
-#     # loop through list of tiles to get tile z/x/y to plug in to Mapillary endpoints and make request
-#     tiles = list(mercantile.tiles(west, south, east, north, 14))
-#     seq_count = 0
-#     img_count = 0
-#     # loop through list of tiles to get tile z/x/y to plug in to Mapillary endpoints and make request
-#     for tile in tiles:
-#         tile_url = 'https://tiles.mapillary.com/maps/vtp/{}/2/{}/{}/{}?access_token={}'.format(tile_coverage, tile.z,
-#                                                                                             tile.x, tile.y,
-#                                                                                             access_token)
-#         response = requests.get(tile_url)
-#         data = vt_bytes_to_geojson(response.content, tile.x, tile.y, tile.z, layer=tile_layer)
-
-#         # push to output geojson object if yes
-#         for feature in data['features']:
-
-#             # get lng,lat of each feature
-#             lng = feature['geometry']['coordinates'][0]
-#             lat = feature['geometry']['coordinates'][1]
-
-#             # ensure feature falls inside bounding box since tiles can extend beyond
-#             if west < lng < east and south < lat < north:
-
-#                 # create a folder for each unique sequence ID to group images by sequence
-#                 sequence_id = feature['properties']['sequence_id']
-#                 if not os.path.exists(os.path.join('data', sequence_id)):
-#                     os.makedirs(os.path.join('data', sequence_id))
-#                     seq_count += 1
-
-#                 # request the URL of each image
-#                 image_id = feature['properties']['id']
-#                 request_image(access_token, sequence_id, image_id)
-
-#     return f"{img_count} images of {seq_count} sequence have been downloaded"
-
+# distance between two points
 def haversine(lon1, lat1, lon2, lat2):
     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
     dlon = lon2 - lon1
     dlat = lat2 - lat1
     a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
-    return 2 * 6371 * asin(sqrt(a))
+    return 2 * 6371 * asin(sqrt(a)) * 1000
+
+def getDegree(lon1, lat1, lon2, lat2):  
+    radLatA = radians(lat1)  
+    radLonA = radians(lon1)  
+    radLatB = radians(lat2)  
+    radLonB = radians(lon2)  
+    dLon = radLonB - radLonA  
+    y = sin(dLon) * cos(radLatB)  
+    x = cos(radLatA) * sin(radLatB) - sin(radLatA) * cos(radLatB) * cos(dLon)  
+    brng = degrees(atan2(y, x))  
+    brng = (brng + 360) % 360  
+    return brng  
 
 
 def find_closest_point(access_token: str, lon, lat):
     tile_coverage = 'mly1_public'
     tile_layer = "image"
+    output= { "type": "FeatureCollection", "features": [] } 
 
 
     west, south, east, north = lon-1e-14, lat-1e-14, lon+1e-14, lat+1e-14
     tiles = list(mercantile.tiles(west, south, east, north, 14))
     
-    closest_feature = None
-    min_distance = float('inf')
-
     # loop through list of tiles to get tile z/x/y to plug in to Mapillary endpoints and make request
     for i, tile in enumerate(tiles):
         tile_url = 'https://tiles.mapillary.com/maps/vtp/{}/2/{}/{}/{}?access_token={}'.format(tile_coverage,tile.z,tile.x,tile.y,access_token)
         response = requests.get(tile_url)
         data = vt_bytes_to_geojson(response.content, tile.x, tile.y, tile.z,layer=tile_layer)
         
+
         for feature in data['features']:
             lon2, lat2 = feature['geometry']['coordinates']
-            if haversine(lon, lat, lon2, lat2) < min_distance and feature['properties']['is_pano']:
-                closest_feature = feature
-                
-    if closest_feature:
-        return request_image(access_token,  closest_feature['properties']['id'])
-    else:
-        return None
-
-
-
-
+            distance = haversine(lon, lat, lon2, lat2)
+            # get panoramic images of distance less than 50 meters
+            if distance < 100 and  feature['properties']['is_pano']:
+                image_url = request_image(access_token,  feature['properties']['id'])
+                feature['img_url'] = image_url
+                feature['degree'] = getDegree(lon, lat, lon2, lat2)
+                output['features'].append(feature)
+    with open('data/images.geojson', 'w') as f:
+        json.dump(output, f)
+    
+    return output
 
 def request_image(access_token, image_id):
     header = {'Authorization': 'OAuth {}'.format(access_token)}
@@ -103,11 +87,4 @@ def request_image(access_token, image_id):
     r = requests.get(url, headers=header)
     data = r.json()
     return data['thumb_2048_url']
-    # save each image with ID as filename to directory by sequence ID
-    # if os.path.isfile('{}/{}.jpg'.format(sequence_id, image_id)):
-    #     return
-
-    # with open('data/{}/{}.jpg'.format(sequence_id, image_id), 'wb') as handler:
-    #     image_data = requests.get(image_url, stream=True).content
-    #     handler.write(image_data)
     
--- server.py
+++ server.py
@@ -6,16 +6,18 @@ from starlette.responses import RedirectResponse
 from query import *
 
 # %%
+client_secret = "MLY|4996748710342229|df37ea75c385590184f067f78962be3a"
+client_id = "4996748710342229"
+
 app = FastAPI()
 templates = Jinja2Templates(directory=".")
 
 def get_access_token(authorization_code: str, old_access_token:str = None):
-    # replace client secret and clinet id with yours
-    header = {"Content-Type": "application/json", "Authorization": "OAuth MLY|4996748710342229|df37ea75c385590184f067f78962be3a"}
+    header = {"Content-Type": "application/json", "Authorization": f"OAuth {client_secret}"}
     if not old_access_token:
-        data = {"grant_type": "authorization_code", "client_id": "4996748710342229", 'code': authorization_code}
+        data = {"grant_type": "authorization_code", "client_id": client_id, 'code': authorization_code}
     else:
-        data = {"grant_type": "authorization_code", "client_id": "4996748710342229", "refresh_token": old_access_token}
+        data = {"grant_type": "authorization_code", "client_id": client_id, "refresh_token": old_access_token}
         
 
     r = requests.post('https://graph.mapillary.com/token', data=data, headers=header)
@@ -28,6 +30,7 @@ def get_access_token(authorization_code: str, old_access_token:str = None):
     return None
 
 
+# get access token for clinet with index.html
 @app.get("/")
 async def root(request: Request, code: str = None, old_access_token: str = None):
     if not code:
@@ -41,14 +44,10 @@ async def root(request: Request, code: str = None, old_access_token: str = None)
         return access_token
     return templates.TemplateResponse('index.html', context={"request": request, "access_token": access_token})
 
-
-
+# 
 @app.get("/api")
 def api(lon: float, lat: float, access_token: str):
-    result = find_closest_point(access_token, lon, lat)
-    if result:
-        return result
-    else:
-        return 
+    return find_closest_point(access_token, lon, lat)
 
+# start the server
 # uvicorn server:app --reload --port 8080
